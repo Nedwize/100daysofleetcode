33a70247fbeec7fa23bd3d7c4afdffd4a2ef397d
NakshatraCodes <nakshatra163@gmail.com>
2021-07-18 00:27:11 +0530

    day 7

--- build/plusOne.js
+++ build/plusOne.js
@@ -1,18 +1,40 @@
 "use strict";
 // 66. Plus One
 function plusOne(digits) {
-    console.log(Number.MAX_SAFE_INTEGER, digits.length);
-    if (digits.length >= Number.MAX_SAFE_INTEGER) {
-        console.log(digits);
+    if (digits.length >= 16) {
+        let i = digits.length - 1;
+        let flag = true;
+        while (i >= 0) {
+            if (digits[i] === 9) {
+                digits[i] = 0;
+            }
+            else {
+                digits[i] = ++digits[i];
+                flag = false;
+                break;
+            }
+            i--;
+        }
+        if (flag === true) {
+            return [1, ...digits];
+        }
+        return digits;
+        // If an integer is greater than MAX_SAFE_INTEGER
+        // Run a while loop from the end of the array to the start
+        // Check if digit is 9 then carry 1
+        // If digit is !9 then add one
     }
-    let num = Number(digits.toString().replace(/,/g, ''));
-    num++;
-    let toNum = (num) => Number(num);
-    var intArr = Array.from(String(num), toNum);
-    return intArr;
+    else {
+        let num = Number(digits.toString().replace(/,/g, ''));
+        num++;
+        let toNum = (num) => Number(num);
+        var intArr = Array.from(String(num), toNum);
+        return intArr;
+    }
 }
-console.log(plusOne([1, 2, 3]));
-console.log(plusOne([4, 3, 2, 1]));
-console.log(plusOne([9]));
-console.log(plusOne([4, 3, 2, 9]));
-console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 4, 3]));
+// console.log(plusOne([1, 2, 3]));
+// console.log(plusOne([4, 3, 2, 1]));
+// console.log(plusOne([9]));
+// console.log(plusOne([4, 3, 2, 9]));
+console.log(plusOne([6, 1, 4, 5, 3, 9, 0, 1, 9, 5, 1, 8, 6, 7, 0, 5, 5, 9, 9]));
+console.log(plusOne([9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9, 9]));
