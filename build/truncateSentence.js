a4a9cc959874831b63eaf1e4de73701e0b095a5e
NakshatraCodes <nakshatra163@gmail.com>
2021-08-06 02:19:52 +0530

    1816 truncate sentence

---
+++ build/truncateSentence.js
@@ -0,0 +1,17 @@
+"use strict";
+// 1816. Truncate Sentence
+function truncateSentence(s, k) {
+    let m = 0;
+    let result = '';
+    for (let i = 0; i < s.length; i++) {
+        if (s[i] === ' ') {
+            m++;
+            if (m === k) {
+                result = s.substring(0, i);
+                break;
+            }
+        }
+    }
+    return result;
+}
+console.log(truncateSentence('Hello how are you Contestant', 4));
---
+++ truncateSentence.ts
@@ -0,0 +1,20 @@
+// 1816. Truncate Sentence
+
+function truncateSentence(s: string, k: number): string {
+  let m = 0;
+  let result = '';
+  s = s + ' ';
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === ' ') {
+      m++;
+
+      if (m === k) {
+        result = s.substring(0, i);
+        break;
+      }
+    }
+  }
+  return result;
+}
+
+console.log(truncateSentence('Hello how are you Contestant', 4));
