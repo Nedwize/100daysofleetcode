01afb1e8f48e1753e848c2f55ba9efc74b2299b6
NakshatraCodes <nakshatra163@gmail.com>
2021-08-06 01:49:01 +0530

    1832 pangram

---
+++ build/sentenceIsPangram.js
@@ -0,0 +1,16 @@
+"use strict";
+// 1832. Check if the Sentence Is Pangram
+function checkIfPangram(sentence) {
+    const alphabetArray = Array.from(Array(26), (_, i) => i + 96 + 1);
+    let resultArray = [];
+    let len = sentence.length;
+    let code = 0;
+    for (let i = 0; i < len; i++) {
+        code = sentence.charCodeAt(i);
+        if (resultArray.indexOf(code) === -1)
+            resultArray.push(code);
+    }
+    resultArray.sort((a, b) => a - b);
+    return resultArray.join('') === alphabetArray.join('');
+}
+console.log(checkIfPangram('thequickbrownfoxjumpsoverthelazydog'));
---
+++ sentenceIsPangram.ts
@@ -0,0 +1,16 @@
+// 1832. Check if the Sentence Is Pangram
+// Rewrite with HashMaps / HashTable
+function checkIfPangram(sentence: string): boolean {
+  const alphabetArray = Array.from(Array(26), (_, i) => i + 96 + 1);
+  let resultArray = [];
+  let len = sentence.length;
+  let code = 0;
+  for (let i = 0; i < len; i++) {
+    code = sentence.charCodeAt(i);
+    if (resultArray.indexOf(code) === -1) resultArray.push(code);
+  }
+  resultArray.sort((a, b) => a - b);
+  return resultArray.join('') === alphabetArray.join('');
+}
+
+console.log(checkIfPangram('thequickbrownfoxjumpsoverthelazydog'));
