986fc466d83b403f8a9b23783f4f8c33dd57ac31
NakshatraCodes <nakshatra163@gmail.com>
2021-08-09 02:57:19 +0530

    1880 summation of two words

---
+++ summationOfTwoWords.ts
@@ -0,0 +1,38 @@
+// 1880. Check if Word Equals Summation of Two Words
+
+function isSumEqual(
+  firstWord: string,
+  secondWord: string,
+  targetWord: string
+): boolean {
+  const getCode = (s: string) => {
+    return (s.charCodeAt(0) - 97).toString();
+  };
+
+  let sum = '';
+  let first = '';
+  let second = '';
+
+  const len = Math.max(firstWord.length, secondWord.length, targetWord.length);
+
+  for (let i = 0; i < len; i++) {
+    if (firstWord[i]) {
+      first += getCode(firstWord[i]);
+    }
+
+    if (secondWord[i]) {
+      second += getCode(secondWord[i]);
+    }
+
+    if (targetWord[i]) {
+      sum += getCode(targetWord[i]);
+    }
+  }
+
+  return Number(first) + Number(second) === Number(sum);
+}
+
+console.log(isSumEqual('acb', 'cba', 'cdb'));
+
+console.log(isSumEqual('aaa', 'a', 'aab'));
+console.log(isSumEqual('aaa', 'a', 'aaaa'));
