2cc51b53d30a583c47cc843562eb872d0ac7ac81
NakshatraCodes <nakshatra163@gmail.com>
2021-08-16 22:43:56 +0530

    remove element

---
+++ removeElement.ts
@@ -0,0 +1,12 @@
+// 27. Remove Element
+
+function removeElement(nums: number[], val: number): number {
+  nums = nums.filter((item) => item !== val);
+  console.log(nums);
+  return nums.length;
+}
+
+// Input: nums = [0,1,2,2,3,0,4,2], val = 2
+// Expected Output = 5
+console.log(removeElement([0, 1, 2, 2, 3, 0, 4, 2], 2));
+console.log(removeElement([3, 2, 2, 3], 3));
