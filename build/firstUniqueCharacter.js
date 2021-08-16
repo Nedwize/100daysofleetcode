0ed28bdc4697226464d6b8866cd3d4ac11e89dac
NakshatraCodes <nakshatra163@gmail.com>
2021-08-17 04:11:13 +0530

    387 first unique character in string

---
+++ build/firstUniqueCharacter.js
@@ -0,0 +1,23 @@
+"use strict";
+// 387. First Unique Character in a String
+// HashMap - Whenever counting or keeping a track is required
+function firstUniqChar(s) {
+    let map = new Map();
+    for (let i = 0; i < s.length; i++) {
+        if (map.has(s[i])) {
+            map.set(s[i], map.get(s[i]) + 1);
+        }
+        else {
+            map.set(s[i], 1);
+        }
+    }
+    for (let i = 0; i < s.length; i++) {
+        if (map.get(s[i]) === 1) {
+            return i;
+        }
+    }
+    return -1;
+}
+console.log(firstUniqChar('leetcode'));
+console.log(firstUniqChar('loveleetcode'));
+console.log(firstUniqChar('aabb'));
