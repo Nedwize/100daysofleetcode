dbe15b34db900b62a387be0553231da530ac6386
NakshatraCodes <nakshatra163@gmail.com>
2021-08-25 15:10:54 +0530

    search in a rotated sorted array

--- cpp/Arrays/BinarySearch/searchInRotatedSortedArray.cpp
+++ cpp/Arrays/BinarySearch/searchInRotatedSortedArray.cpp
@@ -1,5 +1,4 @@
 // 33. Search in Rotated Sorted Array
-// Revisit
 
 #include <iostream>
 #include <vector>
@@ -29,31 +28,40 @@ int binarySearch(vector<int> arr, int target, int start, int end)
 }
 
 
-int findPeakElement(vector<int> arr) {
-      int start = 0;
-      int end = arr.size() - 1;
-      int mid = start + (end - start) / 2;
+int findPivot(vector<int> arr) {
 
-      while(start < end) {
-          mid = start + (end - start) / 2;
+    int start = 0;
+    int end = arr.size() - 1;
+    int mid;
 
-          if (arr[mid] < arr[mid+1]) {
-              start = mid + 1;
-          } else if (arr[mid] > arr[mid+1]) {
-              end = mid;
-          }       
-      }
-      return start;
-};
+    while(start <= end) {
+        mid = start + (end - start) / 2;
+        
+        if(mid < arr.size()-1 && arr[mid] > arr[mid + 1]) {
+            return mid;
+        } else if(mid > 0 && arr[mid - 1] > arr[mid]) {
+            return mid - 1;
+        } else if(arr[mid] > arr[start]) {
+            start = mid + 1;
+        } else {
+            end = end - 1;
+        }
+    }
+    return -1;
+}
 
 int search(vector<int>& nums, int target) {
-    int peak = findPeakElement(nums);
+    int peak = findPivot(nums);
     int indexOfTarget = 0;
+    
+    if(nums[peak] == target) {
+        return peak;
+    }
 
     if(nums[0] > target) {
-        indexOfTarget = binarySearch(nums, target, peak, nums.size() - 1)
+        indexOfTarget = binarySearch(nums, target, peak+1, nums.size() - 1);
     } else {
-        indexOfTarget = binarySearch(nums, target, 0, peak)
+        indexOfTarget = binarySearch(nums, target, 0, peak);
     }
 
     return indexOfTarget;
@@ -62,6 +70,37 @@ int search(vector<int>& nums, int target) {
 int main()
 {
     vector<int> arr = {4,5,6,7,0,1,2};
-    int ind = search(arr, 0);
+    int ind = search(arr, 9);
     cout << "Found at : " << ind << endl;
+}
+
+// Optimized
+
+int search(vector<int>& nums, int target) {
+    
+    int start = 0, end = arr.size() - 1;
+    int mid;
+
+    while(start <= end) {
+        mid = start + (end - start) / 2;
+
+        if(nums[mid] == target) {
+            return mid;
+        }
+
+        if(nums[mid] >= nums[start]) { // Then mid is present at the left side
+            if(target < nums[mid] && target >= nums[start]) {
+                end = end - 1;
+            } else {
+                start = start + 1;
+            }
+        } else { // mid is present at right side
+            if(target > nums[mid] && target <= nums[end]) {
+                start = start + 1;
+            } else {
+                end = end - 1;
+            }
+        }
+    }
+    return -1;
 }
