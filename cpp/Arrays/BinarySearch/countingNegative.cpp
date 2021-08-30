6415b2c0c413af0676f2784d39ef835181009e36
NakshatraCodes <nakshatra163@gmail.com>
2021-08-30 11:03:25 +0530

    update readme

--- cpp/Arrays/BinarySearch/countingNegative.cpp
+++ cpp/Arrays/BinarySearch/countingNegative.cpp
@@ -9,13 +9,12 @@ int binarySearch(vector<int>& arr) {
     int start = 0;
     int end = arr.size() - 1;
     int mid;
-
+    
     while(start <= end) {
         mid = start + (end - start) / 2;
         if(arr[mid] >= 0) {
             start = mid + 1;
         } else {
-            cout << start << mid << end << endl;
             end = mid - 1;
         }
         if(mid == -1) {
@@ -25,10 +24,39 @@ int binarySearch(vector<int>& arr) {
     return start;
 }
 
-int countNegatives(vector<vector<int>>& a) {
+int countNegatives(vector<vector<int>>& arr) {
+    
+    int start = 0;
     int count = 0;
-    for(int i = 0; i < a.size(); i++) {
-        count += a[i].size() - binarySearch(a[i]);
+    int end = arr.size() - 1;
+    int mid;
+
+    while(start <= end) {
+        mid = start + (end - start) / 2;
+        if(arr[mid][0] >= 0) {
+            start = mid + 1;
+        } else {
+            end = mid - 1;
+        }
+    }
+    
+    
+    int ind;
+    if(start == -1) {
+        ind = 0;
+    } else {
+        ind = start;
+    }
+    
+    count += (arr.size() - ind) * arr[0].size();
+    
+    if(arr.size() == 1 && arr[0].size() == 1) {
+        return count;
+    }
+    if(ind == 0) ind = arr.size();
+    
+    for(int i = 0; i < ind; i++) {
+        count += arr[i].size() - binarySearch(arr[i]);
     }
     return count;
 }
