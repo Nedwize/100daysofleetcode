194ad8672cd38f2558e832013c2c2c0ed8e29cdc
NakshatraCodes <nakshatra163@gmail.com>
2021-09-05 04:37:50 +0530

    remove element

---
+++ cpp/Arrays/TwoPointer/removeElement.cpp
@@ -0,0 +1,23 @@
+// author: Nedwize
+// 283. Move Zeroes
+
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+void moveZeroes(vector<int>& nums) {
+
+    int j = 0;
+    int removeElement(vector<int>& nums, int val) {
+        
+        if(nums[i] != val) {
+            nums[j] = nums[i];
+            j++;
+        }
+    }  
+}
+
+int main() {
+
+}
